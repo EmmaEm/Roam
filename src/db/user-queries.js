f0d0aa717299df7b80bfdc69c02ae964b4babaf1
maighdlyn <emmamaighdlyn@gmail.com>
2017-08-21 20:29:28 -0700

    Resolving a minor git mess. Changes reflect previous git commit msg

--- src/db/user-queries.js
+++ src/db/user-queries.js
@@ -3,7 +3,7 @@ const pgp = require('pg-promise')()
 
 const userQueries = {
   createUser: (name, email, password) => {
-    return db.query(`
+    return db.one(`
       INSERT INTO users (name, email, password)
       VALUES ($1, $2, $3)
       RETURNING id`,
