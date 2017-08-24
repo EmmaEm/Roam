4a6639567f50593dab4417309e1607c59e085859
maighdlyn <emmamaighdlyn@gmail.com>
2017-08-24 12:08:56 -0700

    Add current city and join date to user schema

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -8,5 +8,7 @@ CREATE TABLE users (
   name VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL UNIQUE,
   password VARCHAR(255) NOT NULL,
-  picture VARCHAR(255) DEFAULT 'http://www.pieglobal.com/wp-content/uploads/2015/10/placeholder-user.png'
+  picture VARCHAR(255) DEFAULT 'http://www.pieglobal.com/wp-content/uploads/2015/10/placeholder-user.png',
+  current_city VARCHAR(255),
+  join_data DATE DEFAULT CURRENT_DATE
 )
