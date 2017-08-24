64b930ceb5696adad3dc6ee641cf5642dfb7b467
maighdlyn <emmamaighdlyn@gmail.com>
2017-08-24 12:49:05 -0700

    Add username to schema and seed-data

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -6,6 +6,7 @@ CREATE DATABASE roam;
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
+  username VARCHAR(20) NOT NULL UNIQUE,
   email VARCHAR(255) NOT NULL UNIQUE,
   password VARCHAR(255) NOT NULL,
   picture VARCHAR(255) DEFAULT 'http://www.pieglobal.com/wp-content/uploads/2015/10/placeholder-user.png',
