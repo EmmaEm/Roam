84f978666688667242ab2663762f161ff690450e
maighdlyn <emmamaighdlyn@gmail.com>
2017-08-25 10:43:07 -0700

    Middleware checks login status and navbar shows appropriate login/logout options

--- src/server.js
+++ src/server.js
@@ -3,8 +3,7 @@ const app = express()
 const bodyParser = require('body-parser')
 const path = require('path')
 const session = require('express-session')
-const routes = require('./server/routes')
-
+const routes = require('./server/routes')note
 const port = 3000
 
 app.set('view engine', 'ejs')
@@ -14,12 +13,12 @@ app.use(express.static(path.join(__dirname, '../public')))
 
 app.use(bodyParser.urlencoded({extended: false}))
 
-
 const sessionOptions = {
   name: 'session',
   secret: "Chamber of Secrets",
   cookie: { maxAge: 1000 * 60 * 60 * 24}
 }
+
 app.use(session(sessionOptions))
 
 app.use('/', routes)
