9279327718e00db74f0e4c6be0395df950980e57
Ryan Kent <imsolost@gmail.com>
2017-08-29 10:35:21 -0700

    updated seed data to include multiple posts for one city

--- src/db/users-seed-data.sql
+++ src/db/users-seed-data.sql
@@ -1,6 +1,10 @@
 INSERT INTO users (name, username, email, password, current_city) VALUES ('Daenerys', 'Khaleesi', 'dragons@ravens.com', 'Viserion', 'Dragonstone'), ('Jon Snow', 'knownothing', 'kingofthenorth@ravens.com', 'Ghost', 'Winterfell'), ('Arya Stark', 'noone', 'assassin@ravens.com', 'valarmorghulis', 'Winterfell');
 
-INSERT INTO posts (title, city, content, user_id) VALUES ('Icy Wasteland of the Dead', 'The Wall', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', '1'), ('Experience Targaryen History', 'Dragonstone', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', '1'),  ('Too many people', 'King''s Landing', 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', '2');
+INSERT INTO posts (title, city, content, user_id) VALUES
+('Icy Wasteland of the Dead', 'The Wall', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', '1'),
+('Experience Targaryen History', 'Dragonstone', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', '1'),
+('Queen Bitch Lives Here', 'King''s Landing', 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', '1'),
+('Too many people', 'King''s Landing', 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', '2');
 
 INSERT INTO cities (city, state, country) VALUES
 ('The Wall', 'The North', 'Westeros'),
