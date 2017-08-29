9294454a2d8f02e258f66483193e344badb2f3fe
maighdlyn <emmamaighdlyn@gmail.com>
2017-08-28 17:34:09 -0700

    Post page shows city and author with links, profile links to posts, fixes bug in query join

--- src/db/posts.js
+++ src/db/posts.js
@@ -2,7 +2,7 @@ const db = require('./db')
 const pgp = require('pg-promise')()
 
 getByPostId = (id) => {
-  return db.one(`SELECT * FROM posts WHERE post_id = $1`, [id])
+  return db.one(`SELECT * FROM posts JOIN users ON users.user_id = posts.user_id WHERE post_id = $1`, [id])
   .catch((error) => {
     console.log("\nError in posts.getByPostId\n")
     throw error
