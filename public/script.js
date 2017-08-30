5aa60838778a04469afb2c434e2d5c947318f7f1
Ryan Kent <imsolost@gmail.com>
2017-08-29 20:46:24 -0700

    modal complete

--- public/script.js
+++ public/script.js
@@ -1,8 +1,10 @@
 
-showEditView = () => {
-  document.querySelector('.edit').style.zIndex = '1'
-}
+showEditView = () => document.querySelector('.edit').style.zIndex = '1'
+
+showModal = () => document.querySelector('.modal').style.display = 'block'
 
 document.querySelectorAll('.edit-profile').forEach( button => {
   button.addEventListener('click', showEditView )
 })
+
+document.querySelector('.new-post-button').addEventListener('click', showModal )
