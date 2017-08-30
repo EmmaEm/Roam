7eb284e05805984f19345c249bb1250400790131
Ryan Kent <imsolost@gmail.com>
2017-08-30 16:53:31 -0700

    stupid css bullshit

--- public/script.js
+++ public/script.js
@@ -2,9 +2,11 @@
 showEditView = () => document.querySelector('.edit').style.zIndex = '1'
 
 showModal = () => document.querySelector('.modal').style.display = 'block'
+hideModal = () => document.querySelector('.modal').style.display = 'none'
 
 document.querySelectorAll('.edit-profile').forEach( button => {
   button.addEventListener('click', showEditView )
 })
 
-document.querySelector('.new-post-button').addEventListener('click', showModal )
+document.querySelector('.show-post-modal').addEventListener('click', showModal )
+document.querySelector('.hide-post-modal').addEventListener('click', hideModal )
